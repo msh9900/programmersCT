30f2acaf61dd1498bbdac71c291dae534a322e02
MOS <msh9900@naver.com>
2022-12-31 08:54:59 +0900

    Update ct1-34.js

--- ct1-34.js
+++ ct1-34.js
@@ -1,13 +1,25 @@
 //시저 암호
 function solution(s, n) {
-  let arr = s
-    .split("")
-    .map((val) => {
-      return String.fromCharCode(val.charCodeAt(0) + n);
-    })
-    .join("");
-  console.log(arr);
-  var answer = "";
-  return answer;
+    var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lower = "abcdefghijklmnopqrstuvwxyz";
+    var answer= '';
+
+    for(var i =0; i <s.length; i++){
+        var text = s[i];
+        if(text == ' ') {
+            answer += ' '; 
+            continue;
+        }
+        var textArr = upper.includes(text) ? upper : lower;
+        var index = textArr.indexOf(text)+n;
+        if(index >= textArr.length) index -= textArr.length;
+        answer += textArr[index];
+    }
+    return answer;
 }
-solution("AB", 1);
+//다른 사람의 좋은 코드
+
+function solution(s, n) {
+    var chars = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXY                          "
+    return s.split('').map(e => chars[chars.indexOf(e)+n]).join('');
+}
